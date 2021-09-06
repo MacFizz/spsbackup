d75ffbc65c0e720ecc476f0676565a5a247f9ca4
MacFizz <macfizz@apoplexy.net>
2021-09-06 15:25:33 +0200

    Add files via upload

---
+++ post_backup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash#Do some stuff after backing up the datas, unmount a lan drive or something
+echo "Post-backup script..."
+#umount /media/something
+
+echo "...end of post-backup script."
---
+++ pre_backup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Do some stuff before backing up the datas, mount a lan drive or something
+echo "Pre-backup script..."
+#mount /media/somtehing
+
+echo "...end of pre-backup script."
