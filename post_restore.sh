37861cb7497aab83ab38f7cb4e1d5ec7ea733a8f
macfizz <macfizz@apoplexy.net>
2021-09-07 10:14:44 +0100

    Update pre/post restore script

---
+++ post_restore.sh
@@ -0,0 +1,5 @@
+#!/bin/bash#Do some stuff after restoring the datas, unmount a lan drive or something
+echo "Post-restore script..."
+#umount /media/something
+
+echo "...end of post-restore script."
---
+++ pre_restore.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Do some stuff after restoring the datas, mount a lan drive or something
+echo "Pre-restore script..."
+#mount /media/somtehing
+
+echo "...end of pre-restore script."
